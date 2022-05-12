9c25ddb0a5dbd20cadbd5e1afad3004c742e62cf
blevinspercussion <blevinspercussion@gmail.com>
2022-05-12 11:05:08 -0500

    basic js in place for header content, but need to debug css for the nav links

---
+++ src/index.js
@@ -0,0 +1,93 @@
+// Global constants 
+
+const content = document.querySelector('.content');
+const leftSidebar = document.createElement('div');
+const rightSidebar = document.createElement('div');
+const mainContent = document.createElement('div');
+
+
+// Set up basic layout 
+
+// Add classes to main layout
+leftSidebar.classList.add('sidebar');
+leftSidebar.classList.add('left-sidebar');
+rightSidebar.classList.add('sidebar');
+rightSidebar.classList.add('right-sidebar');
+
+// Add basic elements to layout
+content.appendChild(leftSidebar);
+content.appendChild(rightSidebar);
+content.appendChild(mainContent);
+
+// Module for header
+const header = (() => {
+    // Constants for DOM elements
+    const headerDiv = document.createElement('div');
+    const titleText = document.createElement('h1');
+    const tabsDiv = document.createElement('div');
+    const navList = document.createElement('ul');
+    const navListHome = document.createElement('li');
+    const navLinkHome = document.createElement('a');
+    const navListMenu = document.createElement('li');
+    const navLinkMenu = document.createElement('a');
+    const navListAbout = document.createElement('li');
+    const navLinkAbout = document.createElement('a');
+
+
+
+    // Add classes to header DOM elements
+    headerDiv.classList.add('header');
+    titleText.classList.add('title-text');
+    tabsDiv.classList.add('tabs');
+    navList.classList.add('nav');
+    navListHome.classList.add('nav-list-item');
+    navListMenu.classList.add('nav-list-item');
+    navListAbout.classList.add('nav-list-item');
+
+    // Add text to DOM elements
+    titleText.textContent = "Giuseppe's Italian Restaurant";
+    navLinkHome.textContent = 'Home';
+    navLinkMenu.textContent = 'Menu';
+    navLinkAbout.textContent = 'About';
+
+    // Add elements to DOM
+    content.appendChild(headerDiv);
+    content.appendChild()
+    headerDiv.appendChild(titleText);
+    headerDiv.appendChild(tabsDiv);
+    tabsDiv.appendChild(navListHome);
+    tabsDiv.appendChild(navListMenu);
+    tabsDiv.appendChild(navListAbout);
+    navListHome.appendChild(navLinkHome);
+    navListMenu.appendChild(navLinkMenu);
+    navLinkAbout.appendChild(navLinkAbout);
+
+
+});
+
+
+// Module for 'Home' tab 
+const homeTab = (() => {
+    // Constants for DOM elements 
+    
+});
+
+
+
+// Module for 'Menu' tab
+
+
+
+
+// Module for 'About' tab
+
+
+
+
+// Factory function for menu item cards
+
+
+
+
+// Run header model
+header();
